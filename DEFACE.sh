24689617bd85f4d227f5599b70c50d8b7503c822
Bigbos starla <48884075+yordankeres123@users.noreply.github.com>
2019-04-28 17:03:10 +0700

    Add files via upload

---
+++ DEFACE.sh
@@ -0,0 +1,32 @@
+clear
+figlet GREEN | lolcat
+toilet DRAGON | lolcat
+echo
+echo "LOGIN DULU BROO"
+echo
+echo "Kalau gak tau pwnya tanya saya"
+echo
+echo "kontak" | lolcat
+echo "fb Bigbos Starla"
+echo "Ig bigbosstarla" | lolcat
+read -p "USERNAME : " cok
+
+if [ $cok = BIGBOS ]
+then
+clear
+figlet BERHASIL
+echo "Oke username benar"
+apt install python2
+apt install git
+git clone https://github.com/4L13199/LITESCRIPT
+ls
+cd LITESCRIPT
+ls
+python2 LITESCRIPT.py
+fi
+
+if [ $cok =  ]
+then
+clear
+exit
+fi
